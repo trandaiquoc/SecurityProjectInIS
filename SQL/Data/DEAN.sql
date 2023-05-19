fc0ced9ae734485650c80356fdcad8f4037e431e
trandaiquoc <108415687+trandaiquoc@users.noreply.github.com>
2023-05-19 18:03:59 +0700

    Add files via upload

---
+++ SQL/Data/DEAN.sql
@@ -0,0 +1,6 @@
+INSERT INTO DEAN (MADA,TENDA,NGAYBD,PHONG)
+VALUES (101,'Student Supportter',to_date('18/04/2021', 'dd/mm/yyyy'),11);
+INSERT INTO DEAN (MADA,TENDA,NGAYBD,PHONG)
+VALUES (102,'School App Configuration',to_date('08/12/2023', 'dd/mm/yyyy'),12);
+INSERT INTO DEAN (MADA,TENDA,NGAYBD,PHONG)
+VALUES (103,'Data Structure',to_date('23/4/2024', 'dd/mm/yyyy'),13);
